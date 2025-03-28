4b1e28d3c225b12bbf7b1f9630c0c2931f3c0594
xts777 <126665086+xts777@users.noreply.github.com>
2025-03-28 23:29:20 +0900

    confirm: confirm client could fetch data from server side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+      async function fetchData() {
+          try {
+              const response = await fetch("http://localhost:8000/");
+              if (!response.ok) {
+                  throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              const result = await response.json();
+              setData(result);
+          } catch (error) {
+              console.error("Failed to fetch:", error);
+          }
+      }
+      fetchData();
+  }, []);
+
+  console.log("Data fetched:", data);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +36,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React here
         </a>
       </header>
     </div>
