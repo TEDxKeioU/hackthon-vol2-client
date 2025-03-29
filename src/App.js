03346547a1b63e8a3b2ea3b81d6a754b3fa2ae0a
xts777 <126665086+xts777@users.noreply.github.com>
2025-03-29 15:51:22 +0900

    add: add post feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
+  const [submitText, setSubmitText] = useState("");
+  const [inputText, setInputText] = useState("");
 
   useEffect(() => {
       async function fetchData() {
           try {
-              const response = await fetch("http://localhost:8000/");
+              const response = await fetch("http://localhost:8000/todo", {method: "GET"});
               if (!response.ok) {
                   throw new Error(`HTTP error! status: ${response.status}`);
               }
@@ -21,24 +23,45 @@ function App() {
       fetchData();
   }, []);
 
+  useEffect(() => {
+    async function postData() {
+      if (submitText === "" && !submitText) {
+        console.log("submitText is empty && undefined");
+        return;
+      }
+      try {
+        fetch("http://localhost:8000/todo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: submitText,
+            completed: false,
+          }),
+        })
+        console.log("success posts. submitText: ", submitText);
+      }
+      catch (error) {
+        console.error("Failed to post:", error);
+      }
+    }
+    postData();
+  }, [submitText]);
+
   console.log("Data fetched:", data);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here {data ? `: ${data.message}` : ''}
-        </a>
-      </header>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <br />
+        Learn React here {data ? `: ${data[data.length - 1].title}` : ''}
+        <button onClick={() => setSubmitText(inputText)}>submit</button>
     </div>
   );
 }
