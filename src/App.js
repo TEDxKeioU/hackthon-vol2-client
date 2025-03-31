20916278751616a5e4bcd9dba4bdd7c952e7b18f
xts777 <126665086+xts777@users.noreply.github.com>
2025-03-31 17:01:34 +0900

    refactor: resolve conflict and refactor

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-import React, { useEffect, useState } from 'react';
-=======
 import Recipe from './pages/recipe';
 import Home from './pages/home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
->>>>>>> Stashed changes
+
+
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-      async function fetchData() {
-          try {
-              const response = await fetch("http://localhost:8000/");
-              if (!response.ok) {
-                  throw new Error(`HTTP error! status: ${response.status}`);
-              }
-              const result = await response.json();
-              setData(result);
-          } catch (error) {
-              console.error("Failed to fetch:", error);
-          }
-      }
-      fetchData();
-  }, []);
-
-  console.log("Data fetched:", data);
-
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here {data ? `: ${data.message}` : ''}
-        </a>
-      </header>
-    </div>
-  );
-=======
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -56,7 +15,6 @@ function App() {
       </Routes>
     </Router>
   )
->>>>>>> Stashed changes
 }
 
 export default App;
