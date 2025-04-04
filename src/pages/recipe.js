9eec5a6a35b22704b5c4c25ba4a40fb72efd9ad9
xts777 <126665086+xts777@users.noreply.github.com>
2025-04-04 22:58:39 +0900

    add: add return button into recipe page

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-import "../App.css";
+import styles from "../styles/recipe.module.css";
 import Markdown from "react-markdown";
+import { useNavigate } from "react-router-dom";
 
 export default function Recipe() {
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(true);
-  
+  const [initialDelayPassed, setInitialDelayPassed] = useState(false);
+  const navigate = useNavigate();
+
   useEffect(() => {
       async function fetchRecipeData() {
+          if (!initialDelayPassed) {
+              await new Promise(resolve => setTimeout(resolve, 3000)); // Wait for 3 second
+              setInitialDelayPassed(true);
+          }
           if (recipe) {
               console.log("data is not null, no need to fetch again")
               setLoading(false);
@@ -30,17 +37,23 @@ export default function Recipe() {
       fetchRecipeData();
       const interval = setInterval(fetchRecipeData, 1000);
       return () => clearInterval(interval);
-  }, [recipe]);
+  }, [initialDelayPassed, recipe]);
 
   console.log("Data fetched:", recipe);
   return (
-    <div>
-      <h1>レシピ</h1>
-      {loading ? (
-        <p>loading...</p>
-      ) : (
-        <Markdown>{recipe}</Markdown>
-      )}
-    </div>
+    <>
+      <div>
+        <h1>レシピ</h1>
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <Markdown>{recipe}</Markdown>
+        )}
+      </div>
+      <button
+        onClick={() => navigate("/")}
+        className={styles.recipe_button}
+      >戻る</button>
+    </>
   );
 }
